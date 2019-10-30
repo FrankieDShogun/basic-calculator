9d758ee903e25335fe7ec85b37b0969870d5d3c3
FrankieDShogun <frankieh369@gmail.com>
2019-10-30 20:12:01 +0700

    improving calculator design and adding some instruction at basic.js

--- basic.js
+++ basic.js
@@ -1,10 +1,5 @@
-//[one, two, "devide", six, result, remove]
-//1. search the "devide"
-//2. write the number before (ex dev=3; list[0], list[1], list[2])
-//3. do the same after
-//4. search for result
-//5. if result is invoked before anothe function, return the value
-//6. invoke remove to break
-
-
-//adding the command
+// HOW THE ADDITION WORKS
+// 1. GET THE INPUTS
+// 2. EVAL THE INPUTS
+// 3. PRINT OUT THE RESULT WHEN RETURN IS PRESSED
+console.log(eval("12 * 10"));
